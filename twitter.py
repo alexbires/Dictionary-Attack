4ee3a4179d910c92158e9bf5350e6d8716fc91c0
alex bires <alexbires@gmail.com>
2012-12-18 19:26:35 -0500

    Added some basic functionality to twitter.py for the getfriends method

--- twitter.py
+++ twitter.py
@@ -1,4 +1,18 @@
+import urllib
+import json
 #interface
 #spider user
-	#creates a "web" of different users probably a list at the moment
+#creates a "web" of different users probably a list at the moment
+def spider_user(screen_name):
+	url = "http://"
+	pass
 
+def friends(person,limit = 500):
+	url = "http://search.twitter.com/search.json?"
+	url = url + "screen_name=" + person
+	results = urllib.urlopen(url)
+	#results = json.load(results)
+	print results.readlines()
+	pass
+
+friends("dc404")

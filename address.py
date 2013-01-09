ca887e66be82a15000a53b7c715334221a0446b7
alex bires <alexbires@gmail.com>
2013-01-09 18:27:42 -0500

    Ripping lots of things out and starting over

--- address.py
+++ address.py
@@ -1,6 +1,7 @@
 import urllib, sys, os, string
 import argparse
 import json
+import twitter
 
 sub_list = dict()
 userlist = [] #This is for a list of users used by the spideruser method
@@ -49,146 +50,12 @@ def mutateWord(word,fileDict,end=False):
 		files.write(word + "\n")
 	files.close()
 
-def interactive():
-	"""
-	This method guides the user through all of the options that this program
-	has.  This is going to be put on the backburner until at least the twitter
-	part is done with.
-	"""
-	print "Do you have a base dictionary already? [Y/n]"
-	has_dict = raw_input()
-	if (has_dict == "yes"):
-		print "Enter in a file name"
-		raw_file = raw_input()
-	print "Do you have a file of substitions? [Y/n]"
-	has_sub_file = raw_input()
-	if(has_sub_file == "yes"):
-		pass
-
-"""methods for parsing through the twitter json object"""
-def lang(lang):
-	pass
-def to_user_name(name):
-	"""Defines what to do when to_user_name appears as a key value in the 
-	results part of the json object.  This will check if name is in the global
-	list userlist and if not will add it and then spider it"""
-	print name
-	if userlist.count(name)==0:
-		userlist.append(name)
-		spiderUser(name,100)
-		print "userlist is: ",userlist
-	
-def from_user(name):
-	print name,":is from user"
-def user_id(i):
-	pass
-def image(i):
-	pass
-def from_usr_name(i):
-	pass
-def compl():
-	pass
-def max_str():
-	pass
-def since_str():
-	pass
-def page():
-	pass
-def since():
-	pass
-
-def result(intake):
-	#intake is a list
-	s_dict={"iso_language_code":lang,
-			"to_user_name":to_user_name,
-			"to_user_id_str":user_id,
-			"profile_image_url_http":image,
-			"from_user_name":from_usr_name,
-			"from_user":from_user}
-	for x in intake:
-		x_iter = iter(x)
-		print x
-		for i in x_iter:
-			print i
-			i = str(i)
-			#x is a dictionary for the key value pairs of result
-			s_dict[i](str(x[i]))
-
-def spiderUser(user,depth):
-	"""
-	Starts searching twitter with the specified user name.  When results are 
-	returned that contain a different user, that user will be spidered with 
-	the depth setting as depth -1. 
-	If depth is 0, the method will return.
-	"""
-	print "in spider user"
-	if(depth == 0):
-		return
-	if(userlist.count(user) == 0):
-		userlist.append(user)
-	url="http://search.twitter.com/search.json?q=%40"
-	url=url+user
-
-def parseTwitter(user):
-	"""
-	This method is being split up later on but for the moment it is being used
-	to scrape tweets from specific usernames and find words that would be good
-	candidates for the custom dictionary.
-	"""
-	url="http://search.twitter.com/search.json?q=%40"
-	url = url + user
-	results = urllib.urlopen(url)
-	results = json.load(results)
-	iterator = iter(results)
-	switch_dict = {"completed_in":compl,
-				   "max_id_str":max_str,
-				   "since_id_str":since_str,
-				   "page":page,
-				   "since_id":since,}
-	for i in iterator:
-		try:
-			if i == "results":
-				result(results[i])
-			else:
-				switch_dict[i]()
-		except KeyError:
-			pass
-		except TypeError:
-			pass
-	#spiderUser(user,1)
-
-parseTwitter("HuskyStarcraft")
 
 def main():
-	parser = argparse.ArgumentParser()
-	verbinteract = parser.add_mutually_exclusive_group()
-	parser.add_argument("-m","--mutate",
-		help="mutate the word list",action="store_true")
-	parser.add_argument("-s","--substitute",
-		help="specifies a file of mutations",action="store_true")
-	parser.add_argument("subfiles",nargs='?',
-		type=argparse.FileType('r'))
-	parser.add_argument("-e","--end",
-		help="only add the end result of mutations to the dictionary",
-		action="store_true")
-	verbinteract.add_argument("-v","--verbose",
-		help="display verbose output",action="store_true")
-	verbinteract.add_argument("-i","--interactive",
-		help="interactive mode", action="store_true")
-	arguments = parser.parse_args()
-	if arguments.mutate:
-		m=True
-	if arguments.end:
-		end = True
-	subs = arguments.subfiles.name
 	file=open(subs)
 	garbage = file.read()
 	parseSubstitution(garbage)
 	file.close()
-	if arguments.interactive:
-		interactive()
-	#if argv
-	os.system("./" + sys.argv[2] + " " + sys.argv[1] + " -d 1 -w tmp.txt") 
 
 #mutateWord("sponglea","dict.txt")
 def runCewl():
@@ -200,6 +67,7 @@ def runCewl():
 	for x in range(4,len(sys.argv)):
 		cmd += sys.argv[x] + " "
 	os.system(cmd + "> pass.txt")
+
 def mutate():
 	print "Mutations started."
 	f = open("pass.txt","r") 
--- twitter.py
+++ twitter.py
@@ -16,3 +16,29 @@ def friends(person,limit = 500):
 	pass
 
 friends("dc404")
+def parseTwitter(user):
+	"""
+	This method is being split up later on but for the moment it is being used
+	to scrape tweets from specific usernames and find words that would be good
+	candidates for the custom dictionary.
+	"""
+	url="http://search.twitter.com/search.json?q=%40"
+	url = url + user
+	results = urllib.urlopen(url)
+	results = json.load(results)
+	iterator = iter(results)
+	switch_dict = {"completed_in":compl,
+				   "max_id_str":max_str,
+				   "since_id_str":since_str,
+				   "page":page,
+				   "since_id":since,}
+	for i in iterator:
+		try:
+			if i == "results":
+				result(results[i])
+			else:
+				switch_dict[i]()
+		except KeyError:
+			pass
+		except TypeError:
+			pass

fa7b7790079dcaca01bc9d286c007e9e2853bfd6
alex bires <alexbires@gmail.com>
2013-01-16 23:43:31 -0500

    Removed lots of old functions and started over with the mutation engine

--- address.py
+++ address.py
@@ -2,13 +2,14 @@ import urllib, sys, os, string
 import json
 
 sub_list = dict()
-userlist = [] #This is for a list of users used by the spideruser method
 limit = 0
+sub_list["o"]="derpina"
+sub_list["p"]="asf"
 
 def parseSubstitution(sub_string):
 	"""
 	Takes in a string and creates a dictionary to keep track of the mappings
-	that the user specifiesday 8am-5pm, Thur  It returns a dictionary of translations
+	that the user specifies It returns a dictionary of translations
 	The format of the string is string:whatToReplaceWith
 	"""
 	begin=0
@@ -28,37 +29,29 @@ def parseSubstitution(sub_string):
 				sub_list[replace]=rep_with
 	return sub_list
 
-def mutateWordold(word,fileDict,end=False):
-	"""
-	This method takes in a single word as a string and mutates it according
-	to the dictionary global dictionary that has been populated in a previous 
-	method. Adds to the file that is getting saved to.
-	"""
-	files = open(fileDict,'w')
-	iterate = iter(sub_list)
-	for x in iterate:
-		before = word
-		word=word.replace(x,sub_list[x])
-		tofile = word+"\n"
-		if (not before is word) and not end:
-			try:
-				files.write(tofile)
-			except IOError:
-				pass
-	if end:
-		files.write(word + "\n")
-	files.close()
-
 def mutateWord(word,step=False):
-	print word
+	"""
+	This method takes in a word and returns either one of two things
+	If step is true then every step of replacing the word with substitutions
+	will be returned.
+	If step is false, then only the final word will be returned
+	"""
+	toReturn = []
 	iterator = iter(sub_list)
-	for x in iterator:
-		word = word.replace(x,sub_list[x])
+	if step==False:
+		for x in iterator:
+			word = word.replace(x,sub_list[x])
 		print word
+		return word
+	else:
+		for x in iterator:
+			word = word.replace(x,sub_list[x])
+			toReturn.append(word)
+			return toReturn
 
 def main():
-	sub_list["o"]="derpina"
-	mutateWord("hello")
+	sub_list["o"]="h"
+	mutateWord("hello",False)
 	step = False
 	args = sys.argv
 	argLength = len(sys.argv)
@@ -72,9 +65,9 @@ def main():
 				filename = args[i]
 				i+=1
 				wordlist = open(args[i])
-				contents = wordlist.read()
+				contents = wordlist.readlines()#contents is a list
 				wordlist.close()
-			elif args[i] == "-m":
+			elif args[i] == "-m":#the mutation list to open
 				mutateFile = args[i]
 				i+=1
 				mutations = open(args[i])
@@ -90,37 +83,3 @@ def main():
 		except IndexError:
 			pass#need to just end the execution
 main()
-#mutateWord("sponglea","dict.txt")
-def runCewl():
-	print "cewl beginning."
-	#fires off cewl
-	os.system("./" + sys.argv[2] + " " + sys.argv[1] + " -d 1 -w tmp.txt") 
-	print "cewl finished."
-	cmd = "cat tmp.txt "
-	for x in range(4,len(sys.argv)):
-		cmd += sys.argv[x] + " "
-	os.system(cmd + "> pass.txt")
-
-def mutate():
-	print "Mutations started."
-	f = open("pass.txt","r") 
-	words = f.readlines()
-	f.close()
-	print "File read."
-	print str(len(words)) + " words from scrapping."
-	out = words[:]
-	"Numbers appended."
-	for word in words:
-		for num in (range(0,100) + range(1900,2015)):
-			out += [word[:-1] + str(num) + "\n"]
-	words = out[:]
-	print str(len(words)) + " words from first round mutations."
-	for word in out:
-		words += [string.upper(word)]
-		words += [string.capitalize(word)]
-	print str(len(words)) + " words from second round mutations."
-	print "Writing to file."
-	f = open("pass.txt","w")
-	for x in words:
-		f.write(x)
-	f.close()
